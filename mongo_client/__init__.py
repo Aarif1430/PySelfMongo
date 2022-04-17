8297d61b682e65c41318dc1541c643526e07b587
Aarif1430 <malikarif13@gmail.com>
2022-04-17 21:49:33 +0100

    Minor version bump to upload to pypi server

--- mongo_client/__init__.py
+++ mongo_client/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
+__author__ = "Arif Malik"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020, Arif Malik"
+__maintainer__ = "Arif Malik"
+__email__ = "malikarif13@gmail.com"
+__status__ = "Development"
