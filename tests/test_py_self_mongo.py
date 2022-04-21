427dc15d3f1073933237d7921e063b351fa490e3
Aarif1430 <malikarif13@gmail.com>
2022-04-21 22:48:37 +0100

    Documentation wip and fix deploy issue folder error

--- tests/test_py_self_mongo.py
+++ tests/test_py_self_mongo.py
@@ -1,7 +1,7 @@
 from unittest.mock import patch, MagicMock
 
-from PySelfMongo import __version__
-from PySelfMongo.py_self_mongo import PySelfMongo, MongoSingletonClient
+from pyselfmongo import __version__
+from pyselfmongo.py_self_mongo import PySelfMongo, MongoSingletonClient
 import unittest
 
 
@@ -10,7 +10,7 @@ def test_version():
 
 
 class MongoSingletonClientTest(unittest.TestCase):
-    @patch("PySelfMongo.mongo.MongoClient")
+    @patch("pyselfmongo.mongo.MongoClient")
     def test_singleton(self, mock_client):
         client = MongoSingletonClient()
         client.connect()
@@ -19,7 +19,7 @@ class MongoSingletonClientTest(unittest.TestCase):
         new_client.connect()
         self.assertEqual(client, new_client)
 
-    @patch("PySelfMongo.mongo.MongoClient")
+    @patch("pyselfmongo.mongo.MongoClient")
     def test_singleton_with_args(self, mock_client):
         client = MongoSingletonClient(
             db_name="test_db", username="test_user", password="test_pass"
@@ -34,7 +34,7 @@ class MongoSingletonClientTest(unittest.TestCase):
         new_client.connect()
         self.assertEqual(client, new_client)
 
-    @patch("PySelfMongo.mongo.MongoClient")
+    @patch("pyselfmongo.mongo.MongoClient")
     def test_set_db_name(self, mock_client):
         client = MongoSingletonClient()
         client.set_db_name("test_db")
@@ -48,7 +48,7 @@ class MongoSingletonClientTest(unittest.TestCase):
         self.assertEqual(client, new_client)
         self.assertEqual(client.db_name, new_client.db_name, "test_db")
 
-    @patch("PySelfMongo.mongo.MongoClient")
+    @patch("pyselfmongo.mongo.MongoClient")
     def test_set_username(self, mock_client):
         client = MongoSingletonClient()
         client.set_username("test_user")
@@ -62,7 +62,7 @@ class MongoSingletonClientTest(unittest.TestCase):
         self.assertEqual(client, new_client)
         self.assertEqual(client.username, new_client.username, "test_user")
 
-    @patch("PySelfMongo.mongo.MongoClient")
+    @patch("pyselfmongo.mongo.MongoClient")
     def test_set_password(self, mock_client):
         client = MongoSingletonClient()
         client.set_password("test_pass")
